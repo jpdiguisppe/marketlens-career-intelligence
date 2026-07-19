5c6a816625a1049ff25c7bb6f4d17cda1fe578c7
jpdiguiseppe <jpdiguiseppe@widener.edu>
2026-07-19 14:05:50 -0400

    Wire frontend text polish provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { TextPolishProvider } from "./TextPolishProvider";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <TextPolishProvider>
+      <App />
+    </TextPolishProvider>
   </React.StrictMode>,
 );
